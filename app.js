32082119c755e7d4462abbe030cf3c7656c43695
yeye <kambinho123@hotmail.com>
2022-02-11 14:43:25 +0100

    funkar

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const cors = require("cors")
 const express = require("express")
 
 const app = express()
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
 app.use(express.urlencoded({ extended: true }));
