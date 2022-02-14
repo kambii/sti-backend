07347bb3481c095cfc7efa4a233bbf2d6edb82bd
yeye <kambinho123@hotmail.com>
2022-02-14 13:13:57 +0100

    kdkdk

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use('/healthcheck', require('./routes/healthcheck.routes'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));4
 
 app.get("/", (req ,res)=>{ headers={"http_status:200,":200, "cache-control": "no-cache"} 
 body={"status": "available"} 
